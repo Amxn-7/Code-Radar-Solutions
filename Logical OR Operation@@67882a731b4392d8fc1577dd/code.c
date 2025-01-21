d6bd76d9b48048684ee4cd3439986cfa6fa3b1cb
Amxn-7 <amxnsingh07@gmail.com>
2025-01-22 00:11:57 +0530

    Solution to Logical OR Operation

--- Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
+++ Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
@@ -4,9 +4,9 @@ char* welcome() {
     return "Welcome to Code Radar!";
 }
 
-int main() {
+int main(){
     int a , b;
-    scanf("%d %d", &a,&b);
+    scanf("%d %d", &a , &b);
     if(a||b>0){
         printf("True");
     }
