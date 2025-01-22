ff2eb77a185aba1133a9034fd307c21cb6446505
Amxn-7 <amxnsingh07@gmail.com>
2025-01-22 09:47:45 +0530

    Solution to Perform Bitwise AND

--- Perform Bitwise AND@@67882a731b4392d8fc1577e3/code.c
+++ Perform Bitwise AND@@67882a731b4392d8fc1577e3/code.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
-
-char* welcome() {
-    return "Welcome to Code Radar!";
-}
-
 int main() {
     int a,b;
     scanf("%d %d", &a , &b);
-    int c = a && b;
+    int c = a&b;
     printf("%d", c);
     return 0;
 }
