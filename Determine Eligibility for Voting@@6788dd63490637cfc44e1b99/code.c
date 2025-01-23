43ae2d843252849b5162ccce466e87589ba3970f
Amxn-7 <amxnsingh07@gmail.com>
2025-01-23 14:57:57 +0530

    Solution to Determine Eligibility for Voting

---
+++ Determine Eligibility for Voting@@6788dd63490637cfc44e1b99/code.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    int a;
+    scanf("%d ", &a);
+    if(a>=18){
+        printf("Eligible");
+    }
+    else if(a<18){
+        printf("Not Eligible");
+
+    }
+    return 0;
+}
